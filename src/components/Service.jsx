a88af6b8e686fbdd6e86497e747ee5c6421f9ca3
Md Imran Raj <mraj2@opentext.com>
2024-07-12 01:25:14 +0530

    beautified modal

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -7,16 +7,18 @@ const Service = ({data})=>{
         <div className={styles.mainContainer}>
             {/* modal  */}
             <div className="modal fade" id={"exampleModal"+data.id} tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+            <div className={styles.modal + " modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl"}>
                 <div className="modal-content">
-                <div className="modal-header">
+                <div className={styles.modalHeader + " modal-header"}>
                     <h1 className="modal-title fs-5" id="exampleModalLabel">{data.title}</h1>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
                 </div>
-                <div className="modal-body">
-                    ...
+                <div className={styles.modalBody + " modal-body"}>
+                    {data.exampleImg.length !== 0 ?data.exampleImg.map((img,index)=>{
+                        return <img className={styles.exampleImg} key={index} src={img} alt={data.title}></img>
+                    }):<div><img className={styles.sorryImg} src="./images/sorry.png" alt="sorry"/><div className={styles.sorryText}>We'll add it soon...</div></div>}
                 </div>
-                <div className="modal-footer">
+                <div className={styles.modalFooter + " modal-footer"}>
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
                 </div>
