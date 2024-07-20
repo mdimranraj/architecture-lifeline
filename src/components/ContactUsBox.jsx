5499a040eac5730013b94988a1ab39ba45dca4e4
Md Imran Raj <mraj2@opentext.com>
2024-07-20 18:39:43 +0530

    added env

--- src/components/ContactUsBox.jsx
+++ src/components/ContactUsBox.jsx
@@ -15,10 +15,10 @@ const ContactUs = ()=>{
     function sendEmail(e){
         e.preventDefault();
         emailjs.init({
-            publicKey: "cRydnE4ojVEh75a_4",
+            publicKey: process.env.REACT_APP_Public_Key,
           });
-        const SERVICE_ID = "service_ilf1b79"
-        const TEMPLATE_ID = "template_93zexs8"
+        const SERVICE_ID = process.env.REACT_APP_Service_ID;
+        const TEMPLATE_ID = process.env.REACT_APP_Template_ID;
         emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, '#myForm').then(
             (response) => {
                 toast.success('Message Sent !', {
@@ -37,6 +37,7 @@ const ContactUs = ()=>{
               setPhone("");
             },
             (error) => {
+                console.log(error)
               toast.error('Message Failed to Send !', {
                 position: "top-center",
                 autoClose: 5000,
@@ -59,10 +60,9 @@ const ContactUs = ()=>{
                     <h3>We'll reach out to you</h3>
                     <div>
                         Write to us. Give suggetions. Our team will contact you.
-                    </div>
-                    
+                    </div> 
                 </div>
-                <img className={styles.talk} src="./images/talk.gif" alt="contact" />
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.6199671930235!2d77.59003647484292!3d13.059845087263694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae19498f42dbf7%3A0xae3e2917238c6fd1!2sRiseWorx%20Labs%20Pvt%20Ltd%20(CoWork%2C%20SharedOfficeSpace)!5e0!3m2!1sen!2sin!4v1721476975449!5m2!1sen!2sin" title="Location" className={styles.map} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div className={styles.rightPart}>
                 <div>
@@ -84,7 +84,7 @@ const ContactUs = ()=>{
                         </div>
                         <div className={styles.messageDiv}>
                             <label htmlFor="message">Message</label>
-                            <textarea name="message" id="" cols="30" rows="4"  value={message} onChange={event => {setMessage(event.target.value);}}></textarea>
+                            <textarea name="message" id="" rows="4"  value={message} onChange={event => {setMessage(event.target.value);}}></textarea>
                         </div>
                        <button type="submit" className={styles.send + " btn btn-success"}>Send Message</button>
                     </form>
