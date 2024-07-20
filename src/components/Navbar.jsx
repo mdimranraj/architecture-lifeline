c4de61b74254befca7d78e6a95c28c73566d6834
Md Imran Raj <mraj2@opentext.com>
2024-07-20 19:44:15 +0530

    navbar beautified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from '../styles/Navbar.module.css'
 const Navbar = ()=>{
+
+    const location = useLocation();
+    const currentPath = location.pathname;
+
     return(
         <nav className={styles.navbar + " navbar navbar-expand-lg bg-body-tertiary"}>
             <div className="container-fluid">
@@ -14,33 +19,17 @@ const Navbar = ()=>{
                 <div className={styles.navbarNav + " collapse navbar-collapse"} id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    <NavLink className={`nav-link ${currentPath === '/' ? styles.activeLink : ''}`} aria-current="page" to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/services">Services</a>
+                        <NavLink className={`nav-link ${currentPath === '/services' ? styles.activeLink : ''}`} to="/services">Services</NavLink>
                     </li>
-                    {/* <li className="nav-item dropdown">
-                            <div>
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Service
-                                </a>
-                                <ul className={styles.dropdownMenu + " dropdown-menu"}>
-                                    <li><a className="dropdown-item" href="#">Architectural & Specialised Projects</a></li>
-                                    <li><a className="dropdown-item" href="#">Landscaping</a></li>
-                                    <li><a className="dropdown-item" href="#">Interior designing</a></li>
-                                    <li><a className="dropdown-item" href="#">Industrial buildings</a></li>
-                                    <li><a className="dropdown-item" href="#">Township planning</a></li>
-                                    <li><a className="dropdown-item" href="#">Restoration & Conservation</a></li>
-                                    <li><a className="dropdown-item" href="#">Traffic improvement planning</a></li>
-                                    <li><a className="dropdown-item" href="#">Project management consultancy</a></li>
-                                </ul>
-                            </div>
-                        </li> */}
+                    
                     <li className="nav-item">
-                    <a className="nav-link" href="/team">Team</a>
+                    <NavLink className={`nav-link ${currentPath === '/team' ? styles.activeLink : ''}`} to="/team" >Team</NavLink>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/contactus">Contact Us</a>
+                    <NavLink className={`nav-link ${currentPath === '/contactus' ? styles.activeLink : ''}`} to="/contactus">Contact Us</NavLink>
                     </li>
                 </ul>
                 </div>
